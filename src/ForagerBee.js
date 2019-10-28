55ac250bbd2f5cf2695403d0de68c89ed235b326
Kelly Willard <kellywillard@Kellys-MacBook-Pro.local>
2019-10-28 13:52:58 -0700

    Finished individual portion of sprint

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,17 @@
 var ForagerBee = function() {
+  HoneyMakerBee.call(this);
+  this.age = 10;
+  this.job = "find pollen";
+  this.color = "yellow";
+  this.canFly = true;
+  this.treasureChest = [];
 };
+
+ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function(treasure) {
+  return this.treasureChest.push(treasure);
+}
+
+
 
